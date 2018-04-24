fde2d54d10339671764cd2f2e37dac98205f84de
Dallin Boyce <Dallin.Boyce@coxautoinc.com>
2018-04-24 14:33:37 -0600

    100% coverage on v1 tests.

--- tests/v1.test.js
+++ tests/v1.test.js
@@ -1,10 +1,84 @@
-const Stateless = require('./../v1')();
+const Stateless = require("./../v1")();
 
-describe("Stateless v1 Tests", ()=>{
+describe("Stateless v1 Tests", () => {
+  beforeEach(() => {
+    Stateless.clear();
+  });
 
-    it("Adds an event name to the collection", ()=>{
-        Stateless.addEvent("Test Event");
-        expect(Stateless.containsEvent("Test Event")).toBe(true);
+  it("Adds an event name to the collection.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.addEvent("Test Event 2");
+    expect(Stateless.containsEvent("Test Event 2")).toBe(true);
+  });
+
+  it("Adds an event name to the collection async.", () => {
+    Stateless.addEvent("Test Event", () => {
+      expect(Stateless.containsEvent("Test Event")).toBe(true);
     });
+  });
 
+  it("Contains the same events on 2 different instances.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    const newInstance = require("./../v1")();
+    expect(newInstance.containsEvent("Test Event")).toBe(true);
+  });
+
+  it("Doesn't add an event if it already exists.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+  });
+
+  it("Removes an event.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.removeEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(false);
+  });
+
+  it("Doesnt remove anything when its not there.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.removeEvent("Test Event 2");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+  });
+
+  it("Removes an event async.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.removeEvent("Test Event", () => {
+      expect(Stateless.containsEvent("Test Event")).toBe(false);
+    });
+  });
+
+  it("Contains async", () => {
+    Stateless.addEvent("Test Event");
+    Stateless.containsEvent("Test Event", contains => {
+      expect(contains).toBe(true);
+    });
+  });
+
+  it("Clears all events.", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.addEvent("Test Event 2");
+    expect(Stateless.containsEvent("Test Event 2")).toBe(true);
+    Stateless.clear();
+    expect(Stateless.containsEvent("Test Event")).toBe(false);
+    expect(Stateless.containsEvent("Test Event 2")).toBe(false);
+  });
+
+  it("Clears all events. Async", () => {
+    Stateless.addEvent("Test Event");
+    expect(Stateless.containsEvent("Test Event")).toBe(true);
+    Stateless.addEvent("Test Event 2");
+    expect(Stateless.containsEvent("Test Event 2")).toBe(true);
+    Stateless.clear(() => {
+      expect(Stateless.containsEvent("Test Event")).toBe(false);
+      expect(Stateless.containsEvent("Test Event 2")).toBe(false);
+    });
+  });
 });
--- v1.js
+++ v1.js
@@ -1,28 +1,59 @@
-'use strict'
+"use strict";
 
-const HashMap = require('hashmap');
+const HashMap = require("hashmap");
 const eventListeners = new HashMap();
 
-class Stateless{
-
-    /**
-     * @function addEvent Adds an event to the listeners set.
-     * @param {string} eventName The event name.
-     */
-    addEvent(eventName){
-        if(!eventListeners.has(eventName)){
-            eventListeners.set(eventName,[]);
-        }
+class Stateless {
+  /**
+   * @function addEvent Adds an event to be listened to.
+   * @param {string} eventName The event name.
+   * @param {function} callback The callback function if async.
+   */
+  addEvent(eventName, callback) {
+    if (!eventListeners.has(eventName)) {
+      eventListeners.set(eventName, []);
     }
-
-    containsEvent(eventName){
-        return eventListeners.has(eventName);
+    if (callback) {
+      callback();
     }
+  }
 
+  /**
+   * @function containsEvent Returns if the system contains the event by name.
+   * @param {string} eventName The event name.
+   * @param {function} callback The callback function if async.
+   */
+  containsEvent(eventName, callback) {
+    if (callback) {
+      callback(eventListeners.has(eventName));
+    }
+    return eventListeners.has(eventName);
+  }
+
+  /**
+   * @function removeEvent Removes an event.
+   * @param {string} eventName
+   * @param {function} callback
+   */
+  removeEvent(eventName, callback) {
+    if (eventListeners.has(eventName)) {
+      eventListeners.remove(eventName);
+    }
+    if (callback) {
+      callback();
+    }
+  }
+
+  clear(callback) {
+    eventListeners.clear();
+    if (callback) {
+      callback();
+    }
+  }
 }
 
-var initializer = function(){
-    return new Stateless();
-}
+var initializer = function() {
+  return new Stateless();
+};
 
 module.exports = initializer;

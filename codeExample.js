a873ccd0b925b36dc0f3280d9794118c3fdb5264
Dallin Boyce <dboyce@student.neumont.edu>
2018-04-27 07:49:15 -0600

    writing a code example.

--- codeExample.js
+++ codeExample.js
@@ -1,1 +1,24 @@
-var Stateless = require('./src/v1');
+var Stateless = require("./src/statless");
+
+//
+/**
+ * You dispatch events much like actions in redux but they do not directly modify state.
+ * The main difference between an action in redux and event in statless is that you return an object.
+ * The required property on an event is the id the rest is up to you.
+ */
+const NAME = "componentEvent";
+const componentEvent = () => {
+  return {
+    id: NAME,
+    payload: {
+      worksWith: ["Node.js", "React", "Other Javascript Frameworks"]
+    }
+  };
+};
+
+
+function main(){
+
+
+}
+
